47995370c0451e69bced6f9fe03c11695280c27c
hilukaz <lucaskawakami5@gmail.com>
2025-11-30 14:48:04 -0300

    dashboardprocesso

---
+++ src/main/java/sptech/school/DashboardProcesso.java
@@ -0,0 +1,169 @@
+package sptech.school;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DashboardProcesso {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static void main(String[] args) {
+
+        List<String> idEmpresas = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(
+                Dotenv.load().get("DB_URL"), Dotenv.load().get("DB_USER"), Dotenv.load().get("DB_PASSWORD"))) {
+            idEmpresas = ConexaoBd.listaEmpresas(conn);
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar no banco: " + e.getMessage());
+        }
+
+        //  array de empresas
+        List<Map<String, Object>> empresasJson = new ArrayList<>();
+
+        for (String empresa : idEmpresas) {
+
+            List<Map<String, Object>> mainframesJson = new ArrayList<>();
+
+            List<String> dirs = ConexaoAws.listarDiretorios(empresa);
+
+            for (String dir : dirs) {
+
+                String mac = dir.replace("1/", "").replace("/", "");
+
+                //linhas
+                List<String[]> linhas = ConexaoAws.lerArquivoCsvDoTrusted(mac, empresa, "trusted.csv");
+
+                if (linhas.isEmpty()) continue;
+
+                List<TrustedCampos> listaTrusted = new ArrayList<>();
+
+                // pula o cabeçalho
+                for (int i = 1; i < linhas.size(); i++) {
+
+                    String[] registro = linhas.get(i);
+
+                    TrustedCampos trusted = new TrustedCampos();
+
+                    trusted.setMacAdress(registro[0]);
+                    trusted.setTimestamp(registro[1]);
+                    trusted.setIdentificao_mainframe(registro[2]);
+
+                    trusted.setUso_cpu_total_perc(Double.parseDouble(registro[3].replace(",", ".")));
+                    trusted.setUso_ram_total_perc(Double.parseDouble(registro[4].replace(",", ".")));
+                    trusted.setUso_disco_total_perc(Double.parseDouble(registro[5].replace(",", ".")));
+                    trusted.setDisco_throughput_mbs(Double.parseDouble(registro[6].replace(",", ".")));
+                    trusted.setDisco_iops_total(Double.parseDouble(registro[7].replace(",", ".")));
+                    trusted.setDisco_read_count(Integer.parseInt(registro[8] ));
+                    trusted.setDisco_write_count(Integer.parseInt(registro[9] ));
+                    trusted.setDisco_latencia_ms(Double.parseDouble(registro[10].replace(",", ".")));
+
+                    // process 1
+                    trusted.setNome1(registro[11]);
+                    trusted.setCpu_perc1(Double.parseDouble(registro[12].replace(",", ".")));
+                    trusted.setMem_perc1(Double.parseDouble(registro[13].replace(",", ".")));
+
+                    // process 2
+                    trusted.setNome2(registro[14]);
+                    trusted.setCpu_perc2(Double.parseDouble(registro[15].replace(",", ".")));
+                    trusted.setMem_perc2(Double.parseDouble(registro[16].replace(",", ".")));
+
+                    // process 3
+                    trusted.setNome3(registro[17]);
+                    trusted.setCpu_perc3(Double.parseDouble(registro[18].replace(",", ".")));
+                    trusted.setMem_perc3(Double.parseDouble(registro[19].replace(",", ".")));
+
+                    // process 4
+                    trusted.setNome4(registro[20]);
+                    trusted.setCpu_perc4(Double.parseDouble(registro[21].replace(",", ".")));
+                    trusted.setMem_perc4(Double.parseDouble(registro[22].replace(",", ".")));
+
+                    // process 5 (exemplo do seu snippet: indices 23,24,25)
+                    trusted.setNome5(registro[23]);
+                    trusted.setCpu_perc5(Double.parseDouble(registro[24].replace(",", ".")));
+                    trusted.setMem_perc5(Double.parseDouble(registro[25].replace(",", ".")));
+
+                    // process 6
+                    trusted.setNome6(registro[26]);
+                    trusted.setCpu_perc6(Double.parseDouble(registro[27].replace(",", ".")));
+                    trusted.setMem_perc6(Double.parseDouble(registro[28].replace(",", ".")));
+
+                    // process 7
+                    trusted.setNome7(registro[29]);
+                    trusted.setCpu_perc7(Double.parseDouble(registro[30].replace(",", ".")));
+                    trusted.setMem_perc7(Double.parseDouble(registro[31].replace(",", ".")));
+
+                    // process 8
+                    trusted.setNome8(registro[32]);
+                    trusted.setCpu_perc8(Double.parseDouble(registro[33].replace(",", ".")));
+                    trusted.setMem_perc8(Double.parseDouble(registro[34].replace(",", ".")));
+
+                    // process 9
+                    trusted.setNome9(registro[35]);
+                    trusted.setCpu_perc9(Double.parseDouble(registro[36].replace(",", ".")));
+                    trusted.setMem_perc9(Double.parseDouble(registro[37].replace(",", ".")));
+
+                    // process 10
+                    trusted.setNome10(registro[38]);
+                    trusted.setCpu_perc10(Double.parseDouble(registro[39].replace(",", ".")));
+                    trusted.setMem_perc10(Double.parseDouble(registro[40].replace(",", ".")));
+
+                    listaTrusted.add(trusted);
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+    public static String gerarCsvTrusted(List<TrustedCampos> listatrusted) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("macAdress;timestamp;identificao-mainframe;uso_cpu_total_%;uso_ram_total_%;uso_disco_total_%;disco_throughput_mbs;disco_iops_total;disco_read_count;disco_write_count;disco_latencia_ms;nome1;cpu_%1;mem_%1;nome2;cpu_%2;mem_%2;nome3;cpu_%3;mem_%3;nome4;cpu_%4;mem_%4;nome5;cpu_%5;mem_%5;nome6;cpu_%6;mem_%6;nome7;cpu_%7;mem_%7;nome8;cpu_%8;mem_%8;nome9;cpu_%9;mem_%9;\n");
+
+        for (TrustedCampos trusted : listatrusted) {
+
+            sb.append(String.format(
+                    "%s;%s;%s;%.2f;%.2f;%.2f;%.2f;%d;%d;%d;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f;%s;%.2f;%.2f\n",
+                    trusted.getMacAdress(),
+                    trusted.getTimestamp(),
+                    trusted.getIdentificao_mainframe(),
+                    trusted.getUso_cpu_total_perc(),
+                    trusted.getUso_ram_total_perc(),
+                    trusted.getUso_disco_total_perc(),
+                    trusted.getDisco_throughput_mbs(),
+                    trusted.getDisco_iops_total().intValue(),
+                    trusted.getDisco_read_count(),
+                    trusted.getDisco_write_count(),
+                    trusted.getDisco_latencia_ms(),
+
+                    trusted.getNome1(), trusted.getCpu_perc1(), trusted.getMem_perc1(),
+                    trusted.getNome2(), trusted.getCpu_perc2(), trusted.getMem_perc2(),
+                    trusted.getNome3(), trusted.getCpu_perc3(), trusted.getMem_perc3(),
+                    trusted.getNome4(), trusted.getCpu_perc4(), trusted.getMem_perc4(),
+                    trusted.getNome5(), trusted.getCpu_perc5(), trusted.getMem_perc5(),
+                    trusted.getNome6(), trusted.getCpu_perc6(), trusted.getMem_perc6(),
+                    trusted.getNome7(), trusted.getCpu_perc7(), trusted.getMem_perc7(),
+                    trusted.getNome8(), trusted.getCpu_perc8(), trusted.getMem_perc8(),
+                    trusted.getNome9(), trusted.getCpu_perc9(), trusted.getMem_perc9()
+            ));
+
+
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/main/java/sptech/school/TrustedCampos.java
@@ -0,0 +1,407 @@
+package sptech.school;
+
+public class TrustedCampos {
+
+    public void TrustedCampos(){
+
+    }
+
+    private String macAdress;
+    private String timestamp;
+    private String identificao_mainframe;
+    private Double uso_cpu_total_perc;
+    private Double uso_ram_total_perc;
+    private Double uso_disco_total_perc;
+    private Double disco_throughput_mbs;
+    private Double disco_iops_total;
+    private Integer disco_read_count;
+    private Integer disco_write_count;
+    private Double disco_latencia_ms;
+
+    private String nome1; private Double cpu_perc1; private Double mem_perc1;
+    private String nome2; private Double cpu_perc2; private Double mem_perc2;
+    private String nome3; private Double cpu_perc3; private Double mem_perc3;
+    private String nome4; private Double cpu_perc4; private Double mem_perc4;
+    private String nome5; private Double cpu_perc5; private Double mem_perc5;
+    private String nome6; private Double cpu_perc6; private Double mem_perc6;
+    private String nome7; private Double cpu_perc7; private Double mem_perc7;
+    private String nome8; private Double cpu_perc8; private Double mem_perc8;
+    private String nome9; private Double cpu_perc9; private Double mem_perc9;
+    private String nome10; private Double cpu_perc10; private Double mem_perc10;
+
+
+    public String getMacAdress() {
+        return macAdress;
+    }
+
+    public void setMacAdress(String macAdress) {
+        this.macAdress = macAdress;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getIdentificao_mainframe() {
+        return identificao_mainframe;
+    }
+
+    public void setIdentificao_mainframe(String identificao_mainframe) {
+        this.identificao_mainframe = identificao_mainframe;
+    }
+
+    public Double getUso_cpu_total_perc() {
+        return uso_cpu_total_perc;
+    }
+
+    public void setUso_cpu_total_perc(Double uso_cpu_total_perc) {
+        this.uso_cpu_total_perc = uso_cpu_total_perc;
+    }
+
+    public Double getUso_ram_total_perc() {
+        return uso_ram_total_perc;
+    }
+
+    public void setUso_ram_total_perc(Double uso_ram_total_perc) {
+        this.uso_ram_total_perc = uso_ram_total_perc;
+    }
+
+    public Double getUso_disco_total_perc() {
+        return uso_disco_total_perc;
+    }
+
+    public void setUso_disco_total_perc(Double uso_disco_total_perc) {
+        this.uso_disco_total_perc = uso_disco_total_perc;
+    }
+
+    public Double getDisco_throughput_mbs() {
+        return disco_throughput_mbs;
+    }
+
+    public void setDisco_throughput_mbs(Double disco_throughput_mbs) {
+        this.disco_throughput_mbs = disco_throughput_mbs;
+    }
+
+    public Double getDisco_iops_total() {
+        return disco_iops_total;
+    }
+
+    public void setDisco_iops_total(Double disco_iops_total) {
+        this.disco_iops_total = disco_iops_total;
+    }
+
+    public Integer getDisco_read_count() {
+        return disco_read_count;
+    }
+
+    public void setDisco_read_count(Integer disco_read_count) {
+        this.disco_read_count = disco_read_count;
+    }
+
+    public Integer getDisco_write_count() {
+        return disco_write_count;
+    }
+
+    public void setDisco_write_count(Integer disco_write_count) {
+        this.disco_write_count = disco_write_count;
+    }
+
+    public Double getDisco_latencia_ms() {
+        return disco_latencia_ms;
+    }
+
+    public void setDisco_latencia_ms(Double disco_latencia_ms) {
+        this.disco_latencia_ms = disco_latencia_ms;
+    }
+
+    public String getNome1() {
+        return nome1;
+    }
+
+    public void setNome1(String nome1) {
+        this.nome1 = nome1;
+    }
+
+    public Double getCpu_perc1() {
+        return cpu_perc1;
+    }
+
+    public void setCpu_perc1(Double cpu_perc1) {
+        this.cpu_perc1 = cpu_perc1;
+    }
+
+    public Double getMem_perc1() {
+        return mem_perc1;
+    }
+
+    public void setMem_perc1(Double mem_perc1) {
+        this.mem_perc1 = mem_perc1;
+    }
+
+    public String getNome2() {
+        return nome2;
+    }
+
+    public void setNome2(String nome2) {
+        this.nome2 = nome2;
+    }
+
+    public Double getCpu_perc2() {
+        return cpu_perc2;
+    }
+
+    public void setCpu_perc2(Double cpu_perc2) {
+        this.cpu_perc2 = cpu_perc2;
+    }
+
+    public Double getMem_perc2() {
+        return mem_perc2;
+    }
+
+    public void setMem_perc2(Double mem_perc2) {
+        this.mem_perc2 = mem_perc2;
+    }
+
+    public String getNome3() {
+        return nome3;
+    }
+
+    public void setNome3(String nome3) {
+        this.nome3 = nome3;
+    }
+
+    public Double getCpu_perc3() {
+        return cpu_perc3;
+    }
+
+    public void setCpu_perc3(Double cpu_perc3) {
+        this.cpu_perc3 = cpu_perc3;
+    }
+
+    public Double getMem_perc3() {
+        return mem_perc3;
+    }
+
+    public void setMem_perc3(Double mem_perc3) {
+        this.mem_perc3 = mem_perc3;
+    }
+
+    public String getNome4() {
+        return nome4;
+    }
+
+    public void setNome4(String nome4) {
+        this.nome4 = nome4;
+    }
+
+    public Double getCpu_perc4() {
+        return cpu_perc4;
+    }
+
+    public void setCpu_perc4(Double cpu_perc4) {
+        this.cpu_perc4 = cpu_perc4;
+    }
+
+    public Double getMem_perc4() {
+        return mem_perc4;
+    }
+
+    public void setMem_perc4(Double mem_perc4) {
+        this.mem_perc4 = mem_perc4;
+    }
+
+    public String getNome5() {
+        return nome5;
+    }
+
+    public void setNome5(String nome5) {
+        this.nome5 = nome5;
+    }
+
+    public Double getCpu_perc5() {
+        return cpu_perc5;
+    }
+
+    public void setCpu_perc5(Double cpu_perc5) {
+        this.cpu_perc5 = cpu_perc5;
+    }
+
+    public Double getMem_perc5() {
+        return mem_perc5;
+    }
+
+    public void setMem_perc5(Double mem_perc5) {
+        this.mem_perc5 = mem_perc5;
+    }
+
+    public String getNome6() {
+        return nome6;
+    }
+
+    public void setNome6(String nome6) {
+        this.nome6 = nome6;
+    }
+
+    public Double getCpu_perc6() {
+        return cpu_perc6;
+    }
+
+    public void setCpu_perc6(Double cpu_perc6) {
+        this.cpu_perc6 = cpu_perc6;
+    }
+
+    public Double getMem_perc6() {
+        return mem_perc6;
+    }
+
+    public void setMem_perc6(Double mem_perc6) {
+        this.mem_perc6 = mem_perc6;
+    }
+
+    public String getNome7() {
+        return nome7;
+    }
+
+    public void setNome7(String nome7) {
+        this.nome7 = nome7;
+    }
+
+    public Double getCpu_perc7() {
+        return cpu_perc7;
+    }
+
+    public void setCpu_perc7(Double cpu_perc7) {
+        this.cpu_perc7 = cpu_perc7;
+    }
+
+    public Double getMem_perc7() {
+        return mem_perc7;
+    }
+
+    public void setMem_perc7(Double mem_perc7) {
+        this.mem_perc7 = mem_perc7;
+    }
+
+    public String getNome8() {
+        return nome8;
+    }
+
+    public void setNome8(String nome8) {
+        this.nome8 = nome8;
+    }
+
+    public Double getCpu_perc8() {
+        return cpu_perc8;
+    }
+
+    public void setCpu_perc8(Double cpu_perc8) {
+        this.cpu_perc8 = cpu_perc8;
+    }
+
+    public Double getMem_perc8() {
+        return mem_perc8;
+    }
+
+    public void setMem_perc8(Double mem_perc8) {
+        this.mem_perc8 = mem_perc8;
+    }
+
+    public String getNome9() {
+        return nome9;
+    }
+
+    public void setNome9(String nome9) {
+        this.nome9 = nome9;
+    }
+
+    public Double getCpu_perc9() {
+        return cpu_perc9;
+    }
+
+    public void setCpu_perc9(Double cpu_perc9) {
+        this.cpu_perc9 = cpu_perc9;
+    }
+
+    public Double getMem_perc9() {
+        return mem_perc9;
+    }
+
+    public void setMem_perc9(Double mem_perc9) {
+        this.mem_perc9 = mem_perc9;
+    }
+
+    public String getNome10() {
+        return nome10;
+    }
+
+    public void setNome10(String nome10) {
+        this.nome10 = nome10;
+    }
+
+    public Double getCpu_perc10() {
+        return cpu_perc10;
+    }
+
+    public void setCpu_perc10(Double cpu_perc10) {
+        this.cpu_perc10 = cpu_perc10;
+    }
+
+    public Double getMem_perc10() {
+        return mem_perc10;
+    }
+
+    public void setMem_perc10(Double mem_perc10) {
+        this.mem_perc10 = mem_perc10;
+    }
+    public TrustedCampos(){
+
+    }
+
+    public TrustedCampos(String macAdress, String timestamp, String identificao_mainframe, Double uso_cpu_total_perc, Double uso_ram_total_perc, Double uso_disco_total_perc, Double disco_throughput_mbs, Double disco_iops_total, Double disco_read_count, Double disco_write_count, Double disco_latencia_ms, String nome1, Double cpu_perc1, Double mem_perc1, String nome2, Double cpu_perc2, Double mem_perc2, String nome3, Double cpu_perc3, Double mem_perc3, String nome4, Double cpu_perc4, Double mem_perc4, String nome5, Double cpu_perc5, Double mem_perc5, String nome6, Double cpu_perc6, Double mem_perc6, String nome7, Double cpu_perc7, Double mem_perc7, String nome8, Double cpu_perc8, Double mem_perc8, String nome9, Double cpu_perc9, Double mem_perc9, String nome10, Double cpu_perc10, Double mem_perc10) {
+        this.macAdress = macAdress;
+        this.timestamp = timestamp;
+        this.identificao_mainframe = identificao_mainframe;
+        this.uso_cpu_total_perc = uso_cpu_total_perc;
+        this.uso_ram_total_perc = uso_ram_total_perc;
+        this.uso_disco_total_perc = uso_disco_total_perc;
+        this.disco_throughput_mbs = disco_throughput_mbs;
+        this.disco_iops_total = disco_iops_total;
+        this.disco_read_count = disco_read_count;
+        this.disco_write_count = disco_write_count;
+        this.disco_latencia_ms = disco_latencia_ms;
+        this.nome1 = nome1;
+        this.cpu_perc1 = cpu_perc1;
+        this.mem_perc1 = mem_perc1;
+        this.nome2 = nome2;
+        this.cpu_perc2 = cpu_perc2;
+        this.mem_perc2 = mem_perc2;
+        this.nome3 = nome3;
+        this.cpu_perc3 = cpu_perc3;
+        this.mem_perc3 = mem_perc3;
+        this.nome4 = nome4;
+        this.cpu_perc4 = cpu_perc4;
+        this.mem_perc4 = mem_perc4;
+        this.nome5 = nome5;
+        this.cpu_perc5 = cpu_perc5;
+        this.mem_perc5 = mem_perc5;
+        this.nome6 = nome6;
+        this.cpu_perc6 = cpu_perc6;
+        this.mem_perc6 = mem_perc6;
+        this.nome7 = nome7;
+        this.cpu_perc7 = cpu_perc7;
+        this.mem_perc7 = mem_perc7;
+        this.nome8 = nome8;
+        this.cpu_perc8 = cpu_perc8;
+        this.mem_perc8 = mem_perc8;
+        this.nome9 = nome9;
+        this.cpu_perc9 = cpu_perc9;
+        this.mem_perc9 = mem_perc9;
+        this.nome10 = nome10;
+        this.cpu_perc10 = cpu_perc10;
+        this.mem_perc10 = mem_perc10;
+    }
+}
